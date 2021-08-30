167f28463b700797c080216f0c0d6cb5f4a0e93d
wr <werner.randelshofer@siemens.com>
2021-08-30 13:40:51 +0200

    Adds SVG path converters.

--- org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/xml/text/XmlFXSvgPathConverter.java
+++ org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/xml/text/XmlFXSvgPathConverter.java
@@ -4,9 +4,12 @@
  */
 package org.jhotdraw8.xml.text;
 
-import javafx.scene.shape.SVGPath;
+import javafx.scene.shape.PathElement;
 import org.jhotdraw8.annotation.NonNull;
 import org.jhotdraw8.annotation.Nullable;
+import org.jhotdraw8.collection.ImmutableList;
+import org.jhotdraw8.collection.ImmutableLists;
+import org.jhotdraw8.geom.FXSvgPaths;
 import org.jhotdraw8.io.IdResolver;
 import org.jhotdraw8.io.IdSupplier;
 import org.jhotdraw8.text.Converter;
@@ -20,13 +23,13 @@ import java.text.ParseException;
  *
  * @author Werner Randelshofer
  */
-public class XmlSvgPathConverter implements Converter<SVGPath> {
+public class XmlFXSvgPathConverter implements Converter<ImmutableList<PathElement>> {
 
-    public XmlSvgPathConverter() {
+    public XmlFXSvgPathConverter() {
     }
 
     @Override
-    public @Nullable SVGPath fromString(@NonNull CharBuffer buf, @Nullable IdResolver idResolver) throws ParseException, IOException {
+    public @Nullable ImmutableList<PathElement> fromString(@NonNull CharBuffer buf, @Nullable IdResolver idResolver) throws ParseException, IOException {
         CharBuffer out = CharBuffer.allocate(buf.remaining());
         int count = buf.read(out);
         out.position(0);
@@ -35,20 +38,17 @@ public class XmlSvgPathConverter implements Converter<SVGPath> {
         if ("none".equals(string)) {
             return null;
         }
-        SVGPath p = new SVGPath();
-        p.setContent(string);
-        return p;
+        return ImmutableLists.ofCollection(FXSvgPaths.fxPathElementsFromSvgString(string));
     }
 
     @Override
-    public void toString(@NonNull Appendable out, @Nullable IdSupplier idSupplier, @Nullable SVGPath value) throws IOException {
-        final String content = value == null ? null : value.getContent();
+    public <TT extends ImmutableList<PathElement>> void toString(Appendable out, @Nullable IdSupplier idSupplier, @Nullable TT value) throws IOException {
+        final String content = value == null ? null : FXSvgPaths.doubleSvgStringFromElements(value.asList());
         out.append(content == null ? "none" : content);
     }
 
     @Override
-    public @NonNull SVGPath getDefaultValue() {
-        SVGPath p = new SVGPath();
-        return p;
+    public @NonNull ImmutableList<PathElement> getDefaultValue() {
+        return ImmutableLists.emptyList();
     }
 }
