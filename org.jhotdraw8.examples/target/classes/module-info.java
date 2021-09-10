0cb78623a8cf3a438c4fc6456ba70f4aa096c3c6
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2021-09-10 09:34:41 +0200

    Removes files in unwanted directory.

--- org.jhotdraw8.examples/target/classes/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.jhotdraw8.examples {
-    requires java.desktop;
-    requires org.jhotdraw8.draw;
-    requires java.logging;
-    requires java.prefs;
-
-    exports org.jhotdraw8.samples.mini to javafx.graphics, javafx.fxml;
-}
