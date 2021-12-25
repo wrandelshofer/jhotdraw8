d15bd9101f4c6b7d176ca05f6f56cfb4f33dfe8b
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2021-12-25 11:03:38 +0100

    Removes BidiGraphBuilder.

--- org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/graph/MutableBidiGraph.java
+++ org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/graph/MutableBidiGraph.java
@@ -1,4 +1,19 @@
 package org.jhotdraw8.graph;
 
+import org.jhotdraw8.annotation.NonNull;
+
 public interface MutableBidiGraph<V, A> extends BidiGraph<V, A>, MutableDirectedGraph<V, A> {
+    /**
+     * Adds an arrow from 'va' to 'vb' and an arrow from 'vb' to 'va'.
+     *
+     * @param va    vertex a
+     * @param vb    vertex b
+     * @param arrow the arrow
+     */
+    @SuppressWarnings("unused")
+    default void addBidiArrow(@NonNull V va, @NonNull V vb, A arrow) {
+        addArrow(va, vb, arrow);
+        addArrow(vb, va, arrow);
+    }
+
 }
