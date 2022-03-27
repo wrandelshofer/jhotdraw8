ed22d4a56b34d696be7ca3fde2aa8fb27cafc477
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2022-03-27 23:02:26 +0200

    Changes growth factor of primitive lists to factor of 2.

--- org.jhotdraw8.application/src/main/java/org.jhotdraw8.application/org/jhotdraw8/collection/LongArrayList.java
+++ org.jhotdraw8.application/src/main/java/org.jhotdraw8.application/org/jhotdraw8/collection/LongArrayList.java
@@ -8,7 +8,16 @@ import org.jhotdraw8.annotation.NonNull;
 import org.jhotdraw8.annotation.Nullable;
 import org.jhotdraw8.util.Preconditions;
 
-import java.util.*;
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.PrimitiveIterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.function.LongPredicate;
 import java.util.stream.LongStream;
 
@@ -227,7 +236,7 @@ public class LongArrayList extends AbstractList<Long> {
 
     private void grow(int capacity) {
         if (items.length < capacity) {
-            items = ListHelper.grow(size, capacity, 1, items);
+            items = ListHelper.grow(size, Math.max(1, items.length * 2), 1, items);
         }
     }
 
