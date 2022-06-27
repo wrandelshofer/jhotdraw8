6c385a2fddf75c2bba489cf306c94fc3ab5bccb4
Werner Randelshofer <werner.randelshofer@siemens.com>
2022-06-27 18:14:16 +0200

    FXSvgTinyReader renders now for export by default.

--- org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/svg/io/FXSvgTinyReader.java
+++ org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/svg/io/FXSvgTinyReader.java
@@ -7,11 +7,16 @@ package org.jhotdraw8.svg.io;
 
 import javafx.scene.Node;
 import org.jhotdraw8.annotation.NonNull;
+import org.jhotdraw8.collection.typesafekey.MapAccessor;
 import org.jhotdraw8.draw.figure.Figure;
+import org.jhotdraw8.draw.render.RenderContext;
+import org.jhotdraw8.draw.render.RenderingIntent;
 import org.jhotdraw8.draw.render.SimpleDrawingRenderer;
 
 import javax.xml.transform.Source;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Reads an SVG "Tiny" 1.2 file and creates JavaFX nodes from it.
@@ -21,8 +26,16 @@ public class FXSvgTinyReader {
     }
 
     public Node read(@NonNull Source in) throws IOException {
+        Map<MapAccessor<?>, Object> m = new LinkedHashMap<>();
+        RenderContext.RENDERING_INTENT.put(m, RenderingIntent.EXPORT);
+        return read(in, m);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Node read(@NonNull Source in, Map<MapAccessor<?>, Object> renderingHints) throws IOException {
         Figure figure = new FigureSvgTinyReader().read(in);
         SimpleDrawingRenderer r = new SimpleDrawingRenderer();
+        renderingHints.forEach((key, value) -> r.put((MapAccessor<Object>) key, value));
         Node node = r.render(figure);
         node.setManaged(true);
         return node;
