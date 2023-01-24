4e22d7cc6fcc23a383d6b329e17a8406e287f5ca
Werner Randelshofer <werner.randelshofer@siemens.com>
2023-01-24 13:18:01 +0100

    Fix module dependencies.

--- org.jhotdraw8.os/src/main/java/org.jhotdraw8.os/module-info.java
+++ org.jhotdraw8.os/src/main/java/org.jhotdraw8.os/module-info.java
@@ -5,8 +5,11 @@
 
 @SuppressWarnings("module")
 module org.jhotdraw8.os {
+    requires transitive java.xml;
+    requires transitive javafx.graphics;
+    requires transitive org.jhotdraw8.annotation;
+    requires transitive org.jhotdraw8.collection;
+
     exports org.jhotdraw8.os;
     exports org.jhotdraw8.os.macos;
-    requires transitive org.jhotdraw8.annotation;
-    requires transitive java.xml;
 }
