7b2edfc1fb48323219348d753dd17b37be6d77c5
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2023-02-08 16:25:04 +0100

    Fix ArrayIndexOutOfBoundsException in GrowableIntSet8Bit.

--- org.jhotdraw8.collection/src/main/java/org.jhotdraw8.collection/org/jhotdraw8/collection/primitive/GrowableIntSet8Bit.java
+++ org.jhotdraw8.collection/src/main/java/org.jhotdraw8.collection/org/jhotdraw8/collection/primitive/GrowableIntSet8Bit.java
@@ -59,7 +59,7 @@ public class GrowableIntSet8Bit extends DenseIntSet8Bit {
     }
 
     private void ensureCapacity(int index) {
-        if (capacity() < index) {
+        if (capacity() <= index) {
             setCapacity(Integer.highestOneBit(index + index + 1));
         }
     }
