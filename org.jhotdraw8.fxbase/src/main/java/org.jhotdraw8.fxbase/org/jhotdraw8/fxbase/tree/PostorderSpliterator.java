a068736198ca80f5c6e5f4311b05dd369b58677b
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2023-10-16 10:18:49 +0200

    Remove duplicate class PreorderEnumetor (its a duplicate of PreorderSpliterator).

--- org.jhotdraw8.fxbase/src/main/java/org.jhotdraw8.fxbase/org/jhotdraw8/fxbase/tree/PostorderSpliterator.java
+++ org.jhotdraw8.fxbase/src/main/java/org.jhotdraw8.fxbase/org/jhotdraw8/fxbase/tree/PostorderSpliterator.java
@@ -15,7 +15,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * PreorderSpliterator.
+ * PostorderSpliterator.
  *
  * @param <T> the element type
  * @author Werner Randelshofer
