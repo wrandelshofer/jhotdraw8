6387cb319d5f523215452128196e42909572b4e5
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2026-01-26 18:05:20 +0100

    Create jar files with path $publisher/$module/$revision for each file.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,12 +13,16 @@ mvn clean deploy -Dmaven.test.skip=true -Drevision=$revision
 rm -r target/deploy
 for module in org.jhotdraw8.*; do
   echo $module
-  mkdir -p target/deploy/$module
-  cp $module/target/*.jar target/deploy/$module
-  cp $module/.flattened-pom.xml target/deploy/$module/$module-$revision.pom
-  for file in target/deploy/$module/*.jar; do gpg -ab "$file"; done
-  for file in target/deploy/$module/*.pom; do gpg -ab "$file"; done
+  export moduledir=$module/ch/randelshofer/$module/$revision
+  mkdir -p target/deploy/$moduledir
+  cp $module/target/*.jar target/deploy/$moduledir
+  cp $module/.flattened-pom.xml target/deploy/$moduledir/$module-$revision.pom
+  for file in target/deploy/$moduledir/*.{jar,pom}; do
+    gpg -ab "$file";
+    sha1 -r "$file" | cut -f 1 -d " " > "$file.sha1";
+    md5 -r "$file" | cut -f 1 -d " " > "$file.md5";
+  done
   cd target/deploy/$module
-  jar -cfM ../$module-$revision-bundle.jar $(ls -1 $module*|xargs)
+  jar -cfM ../$module-$revision-bundle.jar .
   cd ../../..
 done
